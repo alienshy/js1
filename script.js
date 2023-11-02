008f8f43dcf5016213ea6b49d93abb4a6ef4757e
alienshy <alisahverdiyew8@gmail.com>
2023-11-02 13:21:16 +0400

    jstask

---
+++ script.js
@@ -0,0 +1,35 @@
+let nihad1ceki = 78;
+let nihad1boy = 1.69;
+let nail1ceki = 92;
+let nail1boy = 1.95;
+
+let nihad1BKI = nihad1ceki / (nihad1boy ** 2);
+let nail1BKI = nail1ceki / (nail1boy ** 2);
+
+if(nihad1BKI>nail1BKI){
+    console.log("nihad boyukdu nail")
+
+
+}
+else {
+    console.log("nihad kicikdi nail")
+
+}
+
+let nihad2ceki = 95;
+let nihad2boy = 1.88;
+
+let nail2ceki = 85;
+let nail2boy = 1.76;
+
+let nihad2BKI = nihad2ceki / (nihad2boy ** 2);
+let nail2BKI =nail2ceki / (nail2boy ** 2);
+if(nihad2BKI>nail2BKI){
+    console.log("nihad boyukdu nail")
+
+
+}
+else {
+    console.log("nihad kicikdi nail")
+
+}
